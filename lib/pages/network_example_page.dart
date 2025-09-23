6ceab915cd8eeb8a691a732a6cdde7fd15b6e579
sushant <sushant.khadka@ebpearls.com>
2025-09-23 15:14:05 +0545

    feat(networking):implemented POST http method using dio with defined options and headers

--- lib/pages/network_example_page.dart
+++ lib/pages/network_example_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_app/models/album.dart';
+import 'package:learn_app/models/todos.dart';
 import 'package:learn_app/services/network_service.dart';
 
 class NetworkExamplePage extends StatelessWidget {
@@ -13,35 +13,54 @@ class NetworkExamplePage extends StatelessWidget {
         title: const Text('Networking Example'),
         centerTitle: true,
       ),
-      body: FutureBuilder<Album?>(
-        future: networkService.getRequest(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: const CircularProgressIndicator.adaptive());
-          } else if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
-            final album = snapshot.data;
-            if (album == null) {
-              return const Text("No Album Available");
-            }
-            return Center(
-              child: Column(
-                children: <Widget>[
-                  Text(album.id.toString()),
-                  const SizedBox(height: 12),
-                  Text(album.title),
-                  const SizedBox(height: 12),
-                  Text(album.userId.toString()),
-                  const SizedBox(height: 12),
-                ],
-              ),
-            );
-          }
-          return const SizedBox.shrink();
-        },
+      body: Center(
+        child: Column(
+          children: [
+            // this is the POST method for todos
+            ElevatedButton(
+              onPressed: () async {
+                final message = await networkService.postRequest(Todos.dummy());
+                if (!context.mounted) return;
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text(message!)));
+              },
+              child: const Text('Add Todo'),
+            ),
+          ],
+        ),
       ),
+      // body: FutureBuilder<Album?>(
+      //   future: networkService.getRequest(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.hasError) {
+      //       return Center(child: Text('Error: ${snapshot.error}'));
+      //     } else if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return Center(child: const CircularProgressIndicator.adaptive());
+      //     } else if (snapshot.connectionState == ConnectionState.done &&
+      //         snapshot.hasData) {
+      //       final album = snapshot.data;
+      //       if (album == null) {
+      //         return const Text("No Album Available");
+      //       }
+      //       return Center(
+      //         child: Column(
+      //           children: <Widget>[
+      //             // following is the demo of http method
+      //             // GET method
+      //             Text(album.id.toString()),
+      //             const SizedBox(height: 12),
+      //             Text(album.title),
+      //             const SizedBox(height: 12),
+      //             Text(album.userId.toString()),
+      //             const SizedBox(height: 12),
+      //           ],
+      //         ),
+      //       );
+      //     }
+      //     return const SizedBox.shrink();
+      //   },
+      // ),
     );
   }
 }
--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:learn_app/constants/app_constant.dart';
 import 'package:learn_app/models/album.dart';
 import 'package:learn_app/models/album_extension.dart';
+import 'package:learn_app/models/todos.dart';
 
 class NetworkService {
   NetworkService._private();
@@ -17,7 +19,22 @@ class NetworkService {
     ),
   );
 
-  // http method
+  // defined default headers
+
+  final Map<String, dynamic> _defaultHeaders = {
+    "Content-Type": "application/json",
+  };
+  // define options
+  Options _getOptions({Map<String, dynamic>? headers, String? method}) {
+    final mergedHeaders = <String, dynamic>{};
+    mergedHeaders.addAll(_defaultHeaders);
+    if (headers != null) {
+      mergedHeaders.addAll(headers);
+    }
+    return Options(method: method, headers: mergedHeaders);
+  }
+
+  // http method : GET
   Future<Album?> getRequest() async {
     try {
       final response = await dio.get("/4");
@@ -27,6 +44,27 @@ class NetworkService {
         return albumDomain;
       } else {
         log('Failed to load album. Status code: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      throw Exception("Error occurred during fetching : $e");
+    }
+  }
+
+  // http method : POST
+  Future<String?> postRequest(Todos todos) async {
+    final data = jsonEncode(todos.toMap());
+    log("Data : $data");
+    try {
+      final response = await dio.post(
+        "/todos",
+        data: data,
+        options: _getOptions(method: "POST"),
+      );
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        return "Todo posted successfully!";
+      } else {
+        log("Failed to post todos. Status code: ${response.statusCode}");
         return null;
       }
     } catch (e) {
